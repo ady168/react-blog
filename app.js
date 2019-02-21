0fb3f31a73f83a57fedf25dc14691dca4b1a1656
Ady <ady@Adys-iMac.local>
2019-02-21 05:51:55 -0800

    make it better

---
+++ app.js
@@ -0,0 +1,95 @@
+// jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+
+
+const homeStartingContent = "Welcome to my blog.  We are going to explore more information on how to create a blog using react and templating  thanks for watching! and enjoy!";
+const aboutContent = "This website about page information and description";
+const contactContent = "Contact us for more information"
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
+
+// 1. create mongoose connection
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+// 2. create mongoose schema
+const postSchema = {
+  title: String,
+  content: String
+};
+
+// 3. create new mongoose model
+const Post = mongoose.model("Post", postSchema);
+
+
+
+
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+
+});
+
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+
+  const post = new Post ({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+  post.save();
+
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
