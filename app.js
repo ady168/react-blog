4f22b42b25ac81a1e6c3e72b604222a46de3980c
Ady <ady@Adys-iMac.local>
2019-02-23 06:54:23 -0800

    Updated listen port

--- app.js
+++ app.js
@@ -90,11 +90,7 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
 
-app.listen(port, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000");
 });
